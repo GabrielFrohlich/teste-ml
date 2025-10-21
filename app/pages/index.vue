8b0a05e9950a13f3562a26219ff7119def30bb97
João Gabriel Fröhlich <jfrohlich@GABRIEL-pc.localdomain>
2025-10-20 22:47:52 -0300

    Removido inputs de nome e sobrenome

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,4 @@
 <template>
-    <input type="text" placeholder="nome">
-    <input type="text" placeholder="sobrenome">
     <input type="email" placeholder="email">
     <input type="tel" placeholder="telefone">
 </template>
