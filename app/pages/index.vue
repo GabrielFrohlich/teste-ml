42435f6c43b37043af64a36281ecf434ff5661ce
João Gabriel Fröhlich <jfrohlich@GABRIEL-pc.localdomain>
2025-10-20 22:32:19 -0300

    Inital Commit

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <input type="text" placeholder="nome">
+    <input type="text" placeholder="sobrenome">
+    <input type="email" placeholder="email">
+    <input type="tel" placeholder="telefone">
+</template>
